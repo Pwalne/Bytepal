3c5a554f7a8cac171adf27cf8670a3cadacbb8ae
pjwalne <pwalne12@gmail.com>
2019-06-07 21:22:13 -0400

    test fixed

--- reader_test.go
+++ reader_test.go
@@ -5,15 +5,15 @@ import (
 	"testing"
 )
 
-const(
+const (
 	TestString = "hello2umydarling"
-	Delim = 0
+	Delim      = 0
 )
 
 func TestReadString(t *testing.T) {
 	data := append([]byte(TestString), Delim)
 	reader := NewReader(data)
-	if value := ReadString(Delim); value != TestString {
+	if value := reader.ReadString(Delim); value != TestString {
 		fmt.Printf("Did not decode string correctly, got %s\n", value)
 		t.FailNow()
 	}
@@ -22,11 +22,11 @@ func TestReadString2(t *testing.T) {
 	data := append([]byte(TestString), Delim)
 	data = append(data, data...)
 	reader := NewReader(data)
-	if value := ReadString(Delim); value != TestString {
+	if value := reader.ReadString(Delim); value != TestString {
 		fmt.Printf("Did not decode string correctly, got %s\n", value)
 		t.FailNow()
 	}
-	if value := ReadString(Delim); value != TestString {
+	if value := reader.ReadString(Delim); value != TestString {
 		fmt.Printf("Did not decode second string correctly, got %s\n", value)
 		t.FailNow()
 	}
@@ -35,70 +35,70 @@ func BenchmarkReadBuffer_ReadString(b *testing.B) {
 	data := append([]byte(TestString), Delim)
 	reader := NewReader(data)
 	for i := 0; i < b.N; i++ {
-		_ = ReadString(Delim)
-		currentIndex = 0
+		_ = reader.ReadString(Delim)
+		reader.currentIndex = 0
 	}
 }
 
 func TestReadBuffer_ReadByteLE(t *testing.T) {
 	reader := NewReader([]byte{0x5, 0x8})
-	if ReadUInt8() != 0x5 {
+	if reader.ReadUInt8() != 0x5 {
 		t.FailNow()
 	}
-	if ReadUInt8() != 0x8 {
+	if reader.ReadUInt8() != 0x8 {
 		t.FailNow()
 	}
 }
 func BenchmarkReadBuffer_ReadByteLE(b *testing.B) {
 	reader := NewReader([]byte(TestString))
 	for i := 0; i < b.N; i++ {
-		_ = ReadUInt8()
-		currentIndex = 0
+		_ = reader.ReadUInt8()
+		reader.currentIndex = 0
 	}
 }
 
 func TestReadBuffer_ReadInt16LE(t *testing.T) {
 	reader := NewReader([]byte{0x12, 0x2E})
-	if v:= ReadLEUInt16(); v != 11794 {
-		fmt.Printf("Value: %d\n",v)
+	if v := reader.ReadLEUInt16(); v != 11794 {
+		fmt.Printf("Value: %d\n", v)
 		t.FailNow()
 	}
 }
 func BenchmarkReadBuffer_ReadInt16LE(b *testing.B) {
 	reader := NewReader([]byte(TestString))
 	for i := 0; i < b.N; i++ {
-		_ = ReadLEUInt16()
-		currentIndex = 0
+		_ = reader.ReadLEUInt16()
+		reader.currentIndex = 0
 	}
 }
 
 func TestReadBuffer_ReadIntLE(t *testing.T) {
 	reader := NewReader([]byte{0xDB, 0xAC, 0xCD, 0xB})
-	if v:= ReadLEUInt32(); v != 198028507 {
-		fmt.Printf("Value: %d\n",v)
+	if v := reader.ReadLEUInt32(); v != 198028507 {
+		fmt.Printf("Value: %d\n", v)
 		t.FailNow()
 	}
 }
 func BenchmarkReadBuffer_ReadIntLE(b *testing.B) {
 	reader := NewReader([]byte(TestString))
 	for i := 0; i < b.N; i++ {
-		_ = ReadLEUInt32()
-		currentIndex = 0
+		_ = reader.ReadLEUInt32()
+		reader.currentIndex = 0
 	}
 }
 
 func TestReadBuffer_ReadInt64LE(t *testing.T) {
 	reader := NewReader([]byte{0xCA, 0xBD, 0xFD, 0xCF, 0xCA, 0xBD, 0xCA, 0x3D})
-	if v:= ReadLEUInt64(); v != 4452579860379712970 {
-		fmt.Printf("Value: %d\n",v)
+	if v := reader.ReadLEUInt64(); v != 4452579860379712970 {
+		fmt.Printf("Value: %d\n", v)
 		t.FailNow()
 	}
 }
 func BenchmarkReadBuffer_ReadInt64LE(b *testing.B) {
 	reader := NewReader([]byte(TestString))
 	for i := 0; i < b.N; i++ {
-		_ = ReadLEUInt64()
-		currentIndex = 0
+		_ = reader.ReadLEUInt64()
+		reader.currentIndex = 0
 	}
 }
 
@@ -107,24 +107,24 @@ func TestArrayReader_ReadBits(t *testing.T) {
 		161, 128, 3, 0, 0,
 	}
 	in := NewReader(data)
-	r := ReadBits()
-	if a :=  r(1); a != 1 {
+	r := in.ReadBits()
+	if a := r(1); a != 1 {
 		fmt.Println("bit 1: ", a)
 		t.Fail()
 	}
-	if a :=  r(2); a != 1 {
+	if a := r(2); a != 1 {
 		fmt.Println("Bit 3", a)
 		t.Fail()
 	}
-	if a :=  r(5); a != 1 {
+	if a := r(5); a != 1 {
 		fmt.Println("Bit 8:", a)
 		t.Fail()
 	}
-	if a :=  r(1); a != 1 {
+	if a := r(1); a != 1 {
 		fmt.Println("bit 9: ", a)
 		t.Fail()
 	}
-	if a :=  r(15); a != 3 {
+	if a := r(15); a != 3 {
 		fmt.Println("bit 24: ", a)
 		t.Fail()
 	}
--- writer_test.go
+++ writer_test.go
@@ -5,68 +5,62 @@ import (
 	"testing"
 )
 
-func testBitAccess(t *testing.T, out bitWriter, expected ...int) {
-	fun := BitAccess()
+func testBitAccess(t *testing.T, out Writer, expected ...int) {
+	fun := out.BitAccess()
 	fun(1, 1)
-	if Size() != expected[0] {
-		fmt.Printf("Incorrect payload size. Should be %d, got %d\n", expected[0], Size())
+	if out.Size() != expected[0] {
+		fmt.Printf("Incorrect payload size. Should be %d, got %d\n", expected[0], out.Size())
 		t.Fail()
 	}
 	fun(2, 1)
 	fun(5, 1)
-	if (Payload())[0] != 161 {
+	if (out.Payload())[0] != 161 {
 		t.Fail()
 	}
-	if Size() != expected[1] {
-		fmt.Printf("Incorrect payload size. Should be %d, got %d\n", expected[1], Size())
+	if out.Size() != expected[1] {
+		fmt.Printf("Incorrect payload size. Should be %d, got %d\n", expected[1], out.Size())
 		t.Fail()
 	}
 	fun(1, 1)
-	if (Payload())[1] != 128 {
+	if (out.Payload())[1] != 128 {
 		t.Fail()
 	}
-	if Size() != expected[2] {
-		fmt.Printf("Incorrect payload size. Should be %d, got %d\n", expected[2], Size())
+	if out.Size() != expected[2] {
+		fmt.Printf("Incorrect payload size. Should be %d, got %d\n", expected[2], out.Size())
 		t.Fail()
 	}
 	fun(15, 3)
-	if Size() != expected[3] {
-		fmt.Printf("Incorrect payload size. Should be %d, got %d\n", expected[3], Size())
+	if out.Size() != expected[3] {
+		fmt.Printf("Incorrect payload size. Should be %d, got %d\n", expected[3], out.Size())
 		t.Fail()
 	}
 }
 
-func TestBitWriter_BitAccess1(t *testing.T) {
-	out := bitWriter{
-		bytes: make([]byte, 0),
-	}
+func TestBitWriter_BitAccess2(t *testing.T) {
+	out := NewExpandableWriterWithCap(0)
+	testBitAccess(t, out, 1, 1, 2, 3)
+	out = NewExpandableWriter()
 	testBitAccess(t, out, 1, 1, 2, 3)
 }
-func TestBitWriter_BitAccess2(t *testing.T) {
-	out2 := NewExpandableWriterWithCap(0)
-	testBitAccess(t, *bitWriter, 1, 1, 2, 3)
-	out2 = NewExpandableWriter()
-	testBitAccess(t, *bitWriter, 1, 1, 2, 3)
-}
 func TestBitWriter_BitAccess3(t *testing.T) {
-	out3 := NewFixedWriter(0)
-	testBitAccess(t, *bitWriter, 1, 1, 2, 3)
-	out3 = NewFixedWriter(3)
-	testBitAccess(t, *bitWriter, 3, 3, 3, 3)
+	out := NewFixedWriter(0)
+	testBitAccess(t, out, 1, 1, 2, 3)
+	out = NewFixedWriter(3)
+	testBitAccess(t, out, 3, 3, 3, 3)
 }
 
 func TestFixedWriter_Write(t *testing.T) {
 	out := NewFixedWriter(4)
 	test := []byte{0, 1, 0, 1}
-	Write(test)
+	out.Write(test)
 
-	if Size() != len(test) {
-		fmt.Printf("Incorrect payload size: Got %d, expected %d\n", Size(), len(test))
+	if out.Size() != len(test) {
+		fmt.Printf("Incorrect payload size: Got %d, expected %d\n", out.Size(), len(test))
 	}
 
 	for i := range test {
-		if test[i] != Payload()[i] {
-			fmt.Printf("Got %d Expected %d.\n", Payload()[i], test[i])
+		if test[i] != out.Payload()[i] {
+			fmt.Printf("Got %d Expected %d.\n", out.Payload()[i], test[i])
 			t.FailNow()
 		}
 	}
@@ -74,15 +68,15 @@ func TestFixedWriter_Write(t *testing.T) {
 func TestExpandableWriter_Write(t *testing.T) {
 	out := NewExpandableWriterWithCap(4)
 	test := []byte{0, 1, 0, 1}
-	Write(test)
+	out.Write(test)
 
-	if Size() != len(test) {
-		fmt.Printf("Incorrect payload size: Got %d, expected %d\n", Size(), len(test))
+	if out.Size() != len(test) {
+		fmt.Printf("Incorrect payload size: Got %d, expected %d\n", out.Size(), len(test))
 	}
 
 	for i := range test {
-		if test[i] != Payload()[i] {
-			fmt.Printf("Got %d Expected %d.\n", Payload()[i], test[i])
+		if test[i] != out.Payload()[i] {
+			fmt.Printf("Got %d Expected %d.\n", out.Payload()[i], test[i])
 			t.FailNow()
 		}
 	}
@@ -90,15 +84,15 @@ func TestExpandableWriter_Write(t *testing.T) {
 func TestExpandableWriter_Write2(t *testing.T) {
 	out := NewExpandableWriter()
 	test := []byte{0, 1, 0, 1}
-	Write(test)
+	out.Write(test)
 
-	if Size() != len(test) {
-		fmt.Printf("Incorrect payload size: Got %d, expected %d\n", Size(), len(test))
+	if out.Size() != len(test) {
+		fmt.Printf("Incorrect payload size: Got %d, expected %d\n", out.Size(), len(test))
 	}
 
 	for i := range test {
-		if test[i] != Payload()[i] {
-			fmt.Printf("Got %d Expected %d.\n", Payload()[i], test[i])
+		if test[i] != out.Payload()[i] {
+			fmt.Printf("Got %d Expected %d.\n", out.Payload()[i], test[i])
 			t.FailNow()
 		}
 	}
@@ -107,161 +101,161 @@ func BenchmarkFixedWriter_Write(b *testing.B) {
 	test := []byte(TestString)
 	out := NewFixedWriter(len(test) * b.N)
 	for i := 0; i < b.N; i++ {
-		Write(test)
+		out.Write(test)
 	}
 }
 func BenchmarkExpandableWriter_Write(b *testing.B) {
 	test := []byte(TestString)
 	out := NewExpandableWriterWithCap(len(test) * b.N)
 	for i := 0; i < b.N; i++ {
-		Write(test)
+		out.Write(test)
 	}
 }
 func BenchmarkExpandableWriter_Write2(b *testing.B) {
 	test := []byte(TestString)
 	out := NewExpandableWriter()
 	for i := 0; i < b.N; i++ {
-		Write(test)
+		out.Write(test)
 	}
 }
 
 func TestFixedWriter_WriteUInt8(t *testing.T) {
 	out := NewFixedWriter(1)
-	WriteUInt8(1)
-	if Payload()[0] != 1 {
+	out.WriteUInt8(1)
+	if out.Payload()[0] != 1 {
 		t.FailNow()
 	}
 }
 func TestExpandableWriter_WriteUInt8(t *testing.T) {
 	out := NewExpandableWriterWithCap(1)
-	WriteUInt8(1)
-	if Payload()[0] != 1 {
+	out.WriteUInt8(1)
+	if out.Payload()[0] != 1 {
 		t.FailNow()
 	}
 }
 func TestExpandableWriter_WriteUInt82(t *testing.T) {
 	out := NewExpandableWriter()
-	WriteUInt8(1)
-	if Payload()[0] != 1 {
+	out.WriteUInt8(1)
+	if out.Payload()[0] != 1 {
 		t.FailNow()
 	}
 }
 func BenchmarkFixedWriter_WriteUInt8(b *testing.B) {
 	out := NewFixedWriter(b.N)
 	for i := 0; i < b.N; i++ {
-		WriteUInt8(1)
+		out.WriteUInt8(1)
 	}
 }
 func BenchmarkExpandableWriter_WriteUInt8(b *testing.B) {
 	out := NewExpandableWriterWithCap(b.N)
 	for i := 0; i < b.N; i++ {
-		WriteUInt8(1)
+		out.WriteUInt8(1)
 	}
 }
 func BenchmarkExpandableWriter_WriteUInt82(b *testing.B) {
 	out := NewExpandableWriter()
 	for i := 0; i < b.N; i++ {
-		WriteUInt8(1)
+		out.WriteUInt8(1)
 	}
 }
 
 func TestFixedWriter_WriteUInt8A(t *testing.T) {
 	out := NewFixedWriter(1)
-	WriteUInt8A(2)
+	out.WriteUInt8A(2)
 
-	if Payload()[0] != 130 {
+	if out.Payload()[0] != 130 {
 		t.FailNow()
 	}
 }
 func TestExpandableWriter_WriteUInt8A(t *testing.T) {
 	out := NewExpandableWriterWithCap(1)
-	WriteUInt8A(2)
+	out.WriteUInt8A(2)
 
-	if Size() != 1 {
+	if out.Size() != 1 {
 		t.FailNow()
 	}
-	if Payload()[0] != 130 {
+	if out.Payload()[0] != 130 {
 		t.FailNow()
 	}
 }
 func TestExpandableWriter_WriteUInt8A2(t *testing.T) {
 	out := NewExpandableWriter()
-	WriteUInt8A(2)
+	out.WriteUInt8A(2)
 
-	if Size() != 1 {
+	if out.Size() != 1 {
 		t.FailNow()
 	}
-	if Payload()[0] != 130 {
+	if out.Payload()[0] != 130 {
 		t.FailNow()
 	}
 }
 func BenchmarkFixedWriter_WriteUInt8A(b *testing.B) {
 	out := NewFixedWriter(b.N)
 	for i := 0; i < b.N; i++ {
-		WriteUInt8A(2)
+		out.WriteUInt8A(2)
 	}
 }
 func BenchmarkExpandableWriter_WriteUInt8A(b *testing.B) {
 	out := NewExpandableWriterWithCap(b.N)
 	for i := 0; i < b.N; i++ {
-		WriteUInt8A(2)
+		out.WriteUInt8A(2)
 	}
 }
 func BenchmarkExpandableWriter_WriteUInt8A2(b *testing.B) {
 	out := NewExpandableWriter()
 	for i := 0; i < b.N; i++ {
-		WriteUInt8A(2)
+		out.WriteUInt8A(2)
 	}
 }
 
 func TestFixedWriter_WriteString(t *testing.T) {
 	out := NewFixedWriter(len(TestString) + 1)
-	WriteString(TestString, Delim)
-	if Size() != len(TestString)+1 {
-		fmt.Printf("Size: %d Expected :%d\n", Size(), len(TestString)+1)
+	out.WriteString(TestString, Delim)
+	if out.Size() != len(TestString)+1 {
+		fmt.Printf("Size: %d Expected :%d\n", out.Size(), len(TestString)+1)
 		t.Fail()
 	}
 }
 func TestExpandableWriter_WriteString(t *testing.T) {
 	out := NewExpandableWriterWithCap(len(TestString) + 1)
-	WriteString(TestString, Delim)
-	if Size() != len(TestString)+1 {
-		fmt.Printf("Size: %d Expected :%d\n", Size(), len(TestString)+1)
+	out.WriteString(TestString, Delim)
+	if out.Size() != len(TestString)+1 {
+		fmt.Printf("Size: %d Expected :%d\n", out.Size(), len(TestString)+1)
 		t.Fail()
 	}
 
 	out2 := NewExpandableWriterWithCap(len(TestString) + 1)
-	WriteString(TestString, Delim)
-	if Size() != len(TestString)+1 {
-		fmt.Printf("Size: %d Expected :%d\n", Size(), len(TestString)+1)
+	out2.WriteString(TestString, Delim)
+	if out2.Size() != len(TestString)+1 {
+		fmt.Printf("Size: %d Expected :%d\n", out2.Size(), len(TestString)+1)
 		t.Fail()
 	}
 
-	for i := range Payload() {
-		if Payload()[i] != Payload()[i] {
-			fmt.Printf("Array [%s] != [%s]", string(Payload()), string(Payload()))
+	for i := range out.Payload() {
+		if out.Payload()[i] != out2.Payload()[i] {
+			fmt.Printf("Array [%s] != [%s]", string(out.Payload()), string(out2.Payload()))
 			t.FailNow()
 		}
 	}
 }
 func TestExpandableWriter_WriteString2(t *testing.T) {
 	out := NewExpandableWriter()
-	WriteString(TestString, Delim)
-	if Size() != len(TestString)+1 {
-		fmt.Printf("Size: %d Expected :%d\n", Size(), len(TestString)+1)
+	out.WriteString(TestString, Delim)
+	if out.Size() != len(TestString)+1 {
+		fmt.Printf("Size: %d Expected :%d\n", out.Size(), len(TestString)+1)
 		t.Fail()
 	}
 
 	out2 := NewExpandableWriterWithCap(len(TestString) + 1)
-	WriteString(TestString, Delim)
-	if Size() != len(TestString)+1 {
-		fmt.Printf("Size: %d Expected :%d\n", Size(), len(TestString)+1)
+	out2.WriteString(TestString, Delim)
+	if out2.Size() != len(TestString)+1 {
+		fmt.Printf("Size: %d Expected :%d\n", out2.Size(), len(TestString)+1)
 		t.Fail()
 	}
 
-	for i := range Payload() {
-		if Payload()[i] != Payload()[i] {
-			fmt.Printf("Array [%s] != [%s]", string(Payload()), string(Payload()))
+	for i := range out.Payload() {
+		if out.Payload()[i] != out2.Payload()[i] {
+			fmt.Printf("Array [%s] != [%s]", string(out.Payload()), string(out2.Payload()))
 			t.FailNow()
 		}
 	}
@@ -270,19 +264,19 @@ func BenchmarkFixedWriter_WriteString(b *testing.B) {
 	size := len(TestString) + 1
 	out := NewFixedWriter(size * b.N)
 	for i := 0; i < b.N; i++ {
-		WriteString(TestString, Delim)
+		out.WriteString(TestString, Delim)
 	}
 }
 func BenchmarkExpandableWriter_WriteString(b *testing.B) {
 	size := len(TestString) + 1
 	out := NewExpandableWriterWithCap(size * b.N)
 	for i := 0; i < b.N; i++ {
-		WriteString(TestString, Delim)
+		out.WriteString(TestString, Delim)
 	}
 }
 func BenchmarkExpandableWriter_WriteString2(b *testing.B) {
 	out := NewExpandableWriter()
 	for i := 0; i < b.N; i++ {
-		WriteString(TestString, Delim)
+		out.WriteString(TestString, Delim)
 	}
 }
